9b8645ab5aab8999b7b1b6fbf13c1fa66f4f5177
thebigsmileXD <thebigsmilexd@gmail.com>
2017-02-19 21:55:39 +0100

    Update for raklib, PMMP

--- src/xenialdan/BossBarAPI/SetEntityDataPacket.php
+++ src/xenialdan/BossBarAPI/SetEntityDataPacket.php
@@ -2,20 +2,21 @@
 
 namespace xenialdan\BossBarAPI;
 
-use pocketmine\utils\Binary;
 use pocketmine\network\protocol\DataPacket;
 
-class SetEntityDataPacket extends DataPacket{
+class SetEntityDataPacket extends DataPacket {
 	const NETWORK_ID = 0x27;
+
 	public $eid;
 	public $metadata;
 
-	public function decode(){}
+	public function decode() {
 
-	public function encode(){
+	}
+
+	public function encode() {
 		$this->reset();
 		$this->putEntityId($this->eid);
-		$meta = Binary::writeMetadata($this->metadata);
-		$this->put($meta);
+		$this->putEntityMetadata($this->metadata);
 	}
 }
